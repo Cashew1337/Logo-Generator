9ebc2c2c5c3aab0335b02db21c87ff8b8a13e843
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-02 11:03:33 -0400

    Commit of finished index.js

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-const inquirer = requiure('inquirer');
-const shapes = require('./lib/shapes.js')
+let inquirer = requiure('inquirer');
+let fs = require('fs');
+const generateLogo = require('./lib/shapes');
+const { error } = require('console');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter a 3 character company name/abbreviation.',
+        name: 'text'
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like to use for your logo?',
+        name: 'shape',
+        choices: ['triangle', 'circle', 'rectangle']
+    },
+    {
+        type: 'input',
+        message: 'Please enter a color or color code for the company name.',
+        name: 'color'
+    },
+    {
+        type: 'input',
+        message: 'Please enter a color or color code for the background of the shape.',
+        name: 'background'
+    }
+]
+
+function writeLogo(answers) {
+    console.log(answers);
+    fs.writeFile('logo.svg', answers, (err) => err ? console.error(err) : console.log('Logo generated successfully!'));
+}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        writeLogo(generateLogo(answers))
+    })
+}
+
+init()
